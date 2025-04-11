a6c795714eaa035c33aea7ebeff1ac6fe94e8327
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-04-11 11:54:12 -0400

    Initial rating.js

---
+++ rating/rating.js
@@ -0,0 +1,7 @@
+/* Initial variables for ratings to display correct value/shading when hovering over each.*/
+
+let starRating1;
+let starRating2;
+let starRating3;
+let starRating4;
+let starRating5;
